f5c076dcd85cd2b5e9a999ccbe9c103ad68d7287
周飞 <1174977961@qq.com>
2018-11-25 23:58:28 +0800

    add:添加品牌抽象工厂

---
+++ Objective-C_Design_Patterns/DesignPatterns/抽象工程模式/Apply/ZHFBrandingFactory.h
@@ -0,0 +1,17 @@
+//
+//  ZHFBrandingFactory.h
+//  Objective-C_Design_Patterns
+//
+//  Created by zhoufei on 2018/11/25.
+//  Copyright © 2018年 周飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHFBrandingFactory : NSObject
+- (instancetype)factory;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C_Design_Patterns/DesignPatterns/抽象工程模式/Apply/ZHFBrandingFactory.m
@@ -0,0 +1,13 @@
+//
+//  ZHFBrandingFactory.m
+//  Objective-C_Design_Patterns
+//
+//  Created by zhoufei on 2018/11/25.
+//  Copyright © 2018年 周飞. All rights reserved.
+//
+
+#import "ZHFBrandingFactory.h"
+
+@implementation ZHFBrandingFactory
+
+@end
