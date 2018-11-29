1f54737c9a5ccb17dfc00cf3aaca2a894bc9fcfa
周飞 <1174977961@qq.com>
2018-11-29 23:58:15 +0800

    add:添加角色对象创建执行者类

---
+++ Objective-C_Design_Patterns/DesignPatterns/生成器模式/Apply/ZHFCharacteBuilder.h
@@ -0,0 +1,18 @@
+//
+//  ZHFCharacteBuilder.h
+//  Objective-C_Design_Patterns
+//
+//  Created by zhoufei on 2018/11/29.
+//  Copyright © 2018年 周飞. All rights reserved.
+//
+//  根据规则创建对应的角色模型，规则的执行者
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHFCharacteBuilder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C_Design_Patterns/DesignPatterns/生成器模式/Apply/ZHFCharacteBuilder.m
@@ -0,0 +1,13 @@
+//
+//  ZHFCharacteBuilder.m
+//  Objective-C_Design_Patterns
+//
+//  Created by zhoufei on 2018/11/29.
+//  Copyright © 2018年 周飞. All rights reserved.
+//
+
+#import "ZHFCharacteBuilder.h"
+
+@implementation ZHFCharacteBuilder
+
+@end
