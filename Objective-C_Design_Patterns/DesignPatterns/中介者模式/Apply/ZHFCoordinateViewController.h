5513389aebdbb1ce049096867c25216966a2144a
周飞 <1174977961@qq.com>
2018-12-10 23:03:45 +0800

    add:添加画板中介者模式

--- Objective-C_Design_Patterns/DesignPatterns/中介者模式/Apply/ZHFCoordinateViewController.h
+++ Objective-C_Design_Patterns/DesignPatterns/中介者模式/Apply/ZHFCoordinateViewController.h
@@ -6,12 +6,24 @@
 //  Copyright © 2018年 周飞. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <BottomComponentLib/Macro.h>
+#import "CanvasViewController.h"
+#import "ThumbnailViewController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ZHFCoordinateViewController : UIViewController
+@interface ZHFCoordinateViewController : NSObject
+{
+    @private
+    CanvasViewController *canvasViewController_;
+    UIViewController *activeViewController_;
+}
+SingletonH(ZHFCoordinateViewController)
+@property (nonatomic, strong, readonly) CanvasViewController *canvasViewController;
+@property (nonatomic, strong, readonly) UIViewController *activeViewController;
 
+- (void)requestViewChangeByObject:(id)object;
 @end
 
 NS_ASSUME_NONNULL_END
--- Objective-C_Design_Patterns/DesignPatterns/中介者模式/Apply/ZHFCoordinateViewController.m
+++ Objective-C_Design_Patterns/DesignPatterns/中介者模式/Apply/ZHFCoordinateViewController.m
@@ -13,20 +13,11 @@
 @end
 
 @implementation ZHFCoordinateViewController
+SingletonM(ZHFCoordinateViewController)
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)requestViewChangeByObject:(id)object {
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
