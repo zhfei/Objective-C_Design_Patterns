02428f86269fed557a0a8aa85ae0a9d7b667fe24
周飞 <1174977961@qq.com>
2019-03-28 19:06:55 +0800

    add: 添加单例模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/单例模式/DPSingleViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPSingleViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/3/28.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPSingleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/单例模式/DPSingleViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPSingleViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/3/28.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPSingleViewController.h"
+
+@interface DPSingleViewController ()
+
+@end
+
+@implementation DPSingleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
