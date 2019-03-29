ebe9624b947b202c02068979a79e0a47a51ece1a
周飞 <1174977961@qq.com>
2019-03-29 22:59:47 +0800

    add: 添加原型模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/原型模式/DPPrototypeViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPPrototypeViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/3/29.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPPrototypeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
