d711a9131c9a4a3cce6831e2606d5e342b8fd1b0
周飞 <1174977961@qq.com>
2019-04-09 23:36:22 +0800

    add: 添加抽象工厂模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/抽象工厂模式/DPAbstractFactoryViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPAbstractFactoryViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/9.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPAbstractFactoryViewController : DPBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/抽象工厂模式/DPAbstractFactoryViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPAbstractFactoryViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/9.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPAbstractFactoryViewController.h"
+
+@interface DPAbstractFactoryViewController ()
+
+@end
+
+@implementation DPAbstractFactoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
