6a1905881a40097bf67c8fc2089b2c5c46cbd634
周飞 <1174977961@qq.com>
2019-04-14 23:57:48 +0800

    add: 添加单例模式类

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/单例模式/DPSingleObject.h
@@ -0,0 +1,17 @@
+//
+//  DPSingleObject.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/14.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPSingleObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/单例模式/DPSingleObject.m
@@ -0,0 +1,13 @@
+//
+//  DPSingleObject.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/14.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPSingleObject.h"
+
+@implementation DPSingleObject
+
+@end
