020a9fc982d24d3d368201a02ae18a972ccdc22c
周飞 <1174977961@qq.com>
2019-04-18 22:56:34 +0800

    add: 添加构建者子类

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/Builder/DPStandardCharacterBuilder.h
@@ -0,0 +1,17 @@
+//
+//  DPStandardCharacterBuilder.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/18.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPCharacterBuilder.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPStandardCharacterBuilder : DPCharacterBuilder
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/Models/DPCharacter.h
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/Models/DPCharacter.h
@@ -7,11 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DPCharacter : NSObject
-@property (nonatomic, assign) CGFloat protection;
+@property (nonatomic, assign) CGFloat protection;//防御
+@property (nonatomic, assign) CGFloat power;//攻击
+@property (nonatomic, assign) CGFloat strength;//力量
+@property (nonatomic, assign) CGFloat stamina;//耐力
+@property (nonatomic, assign) CGFloat intelligence;//智力
+@property (nonatomic, assign) CGFloat agility;//敏捷
+@property (nonatomic, assign) CGFloat aggressiveness;//攻击力
 @end
 
 NS_ASSUME_NONNULL_END
