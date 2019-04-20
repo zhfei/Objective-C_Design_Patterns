f093761451649a740c0015a448f8b4c22b1b5a2d
周飞 <1174977961@qq.com>
2019-04-20 23:53:01 +0800

    update:完善指挥者

--- Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/Director/DPChasingGame.m
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/Director/DPChasingGame.m
@@ -9,5 +9,22 @@
 #import "DPChasingGame.h"
 
 @implementation DPChasingGame
-
+- (DPCharacter *)createPlayer:(DPCharacterBuilder *)builder {
+    [builder buildNewCharacter];
+    [builder buildStrength:0.2];
+    [builder buildStamina:0.3];
+    [builder buildIntelligence:0.4];
+    [builder buildAgility:0.5];
+    [builder buildAggressiveness:0.6];
+    return builder.character;
+}
+- (DPCharacter *)createEnemy:(DPCharacterBuilder *)builder {
+    [builder buildNewCharacter];
+    [builder buildStrength:0.3];
+    [builder buildStamina:0.8];
+    [builder buildIntelligence:0.3];
+    [builder buildAgility:0.56];
+    [builder buildAggressiveness:0.87];
+    return builder.character;
+}
 @end
