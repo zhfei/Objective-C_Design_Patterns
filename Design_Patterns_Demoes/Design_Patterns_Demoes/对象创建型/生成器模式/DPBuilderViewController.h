7ff209561029841db488dffcd292a5882d06f61c
周飞 <1174977961@qq.com>
2019-04-20 23:58:38 +0800

    add: 添加构建者VC

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/DPBuilderViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPBuilderViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/20.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPBuilderViewController : DPBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/对象创建型/生成器模式/DPBuilderViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPBuilderViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/4/20.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPBuilderViewController.h"
+
+@interface DPBuilderViewController ()
+
+@end
+
+@implementation DPBuilderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
