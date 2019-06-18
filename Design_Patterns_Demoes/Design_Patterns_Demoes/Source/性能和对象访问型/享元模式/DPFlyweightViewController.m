b8e1cd0ac80483d440428fe801ae111e86c65828
周飞 <1174977961@qq.com>
2019-06-18 22:51:52 +0800

    other:设置UI

--- Design_Patterns_Demoes/Design_Patterns_Demoes/Source/性能和对象访问型/享元模式/DPFlyweightViewController.m
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/性能和对象访问型/享元模式/DPFlyweightViewController.m
@@ -7,17 +7,68 @@
 //
 
 #import "DPFlyweightViewController.h"
+#import "DPFlyweightFactory.h"
 
 @interface DPFlyweightViewController ()
 
 @end
 
 @implementation DPFlyweightViewController
-
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self addUI];
+    [self setupUI];
+    [self setupLayout];
+    [self setupData];
+    [self initData];
 }
+
+#pragma mark - Private Method
+
+- (void)addUI {
+    
+}
+
+- (void)setupUI {
+    self.view.backgroundColor = [UIColor whiteColor];
+    NSMutableArray *pool = @[].mutableCopy;
+    
+    for (NSInteger i = 0; i < 5000; i++) {
+        DPFlyweightModel *flyweight = [[DPFlyweightFactory sharedFlyweightFactory] flyweightModelWithName:@""];
+        flyweight.area = CGRectMake(arc4random_uniform(320), arc4random_uniform(480), arc4random_uniform(320),  arc4random_uniform(480));
+        [pool addObject:flyweight];
+    }
+
+}
+
+- (void)setupLayout {
+    
+}
+
+- (void)initData {
+    
+}
+
+- (void)setupData {
+    
+}
+
+// MARK: overwrite
+
+#pragma mark - Public Method
+
+#pragma mark - Event
+
+#pragma mark - Delegate
+
+#pragma mark - Getter, Setter
+
+#pragma mark - NSCopying
+
+#pragma mark - NSObject
+
 
 
 @end
