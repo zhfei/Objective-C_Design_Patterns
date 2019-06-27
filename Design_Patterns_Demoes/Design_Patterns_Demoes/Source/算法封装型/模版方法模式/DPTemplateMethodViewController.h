3bddc2983f4f73e73dc59e588d18eb710e1b9480
周飞 <1174977961@qq.com>
2019-06-27 23:29:52 +0800

    add: 添加方法模版模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/算法封装型/模版方法模式/DPTemplateMethodViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPTemplateMethodViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/6/27.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPTemplateMethodViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/算法封装型/模版方法模式/DPTemplateMethodViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPTemplateMethodViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/6/27.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPTemplateMethodViewController.h"
+
+@interface DPTemplateMethodViewController ()
+
+@end
+
+@implementation DPTemplateMethodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
