57536b6efadbccbd9f9d2049eb1948440c13f5e7
周飞 <1174977961@qq.com>
2019-06-28 23:54:12 +0800

    add: 添加模版方法模型类

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/算法封装型/模版方法模式/Model/DPTemplateModel.h
@@ -0,0 +1,17 @@
+//
+//  DPTemplateModel.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/6/28.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPTemplateModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/算法封装型/模版方法模式/Model/DPTemplateModel.m
@@ -0,0 +1,26 @@
+//
+//  DPTemplateModel.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/6/28.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPTemplateModel.h"
+
+@implementation DPTemplateModel
+- (void)action {
+    [self step1];
+    [self step2];
+}
+
+- (void)step1 {
+    
+}
+
+- (void)step2 {
+    
+}
+
+
+@end
