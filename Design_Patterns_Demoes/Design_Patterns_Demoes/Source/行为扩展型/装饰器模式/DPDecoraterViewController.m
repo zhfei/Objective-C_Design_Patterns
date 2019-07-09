5b6e0a45ce30b5cb87a2b920e609cd8022e9fad2
周飞 <1174977961@qq.com>
2019-07-09 23:06:44 +0800

    other:使用装饰器

--- Design_Patterns_Demoes/Design_Patterns_Demoes/Source/行为扩展型/装饰器模式/DPDecoraterViewController.m
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/行为扩展型/装饰器模式/DPDecoraterViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "DPDecoraterViewController.h"
+#import "DPImageFilterObject1.h"
+#import "DPImageComponentObject1.h"
 
 @interface DPDecoraterViewController ()
 
@@ -17,16 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    DPImageComponentObject1 *imageObject = [DPImageComponentObject1 new];
+    
+    //添加装饰器filterObject
+    DPImageFilterObject1 *filterObject = [DPImageFilterObject1 imageFilter:imageObject];
+    [filterObject operation];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
