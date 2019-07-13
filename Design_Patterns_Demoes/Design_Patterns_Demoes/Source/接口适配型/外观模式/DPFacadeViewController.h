a1e6a5484ea5f7ffeafa74701a8205025b9d1d38
周飞 <1174977961@qq.com>
2019-07-13 23:11:40 +0800

    add: 添加外观模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/接口适配型/外观模式/DPFacadeViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPFacadeViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/7/13.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPFacadeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
