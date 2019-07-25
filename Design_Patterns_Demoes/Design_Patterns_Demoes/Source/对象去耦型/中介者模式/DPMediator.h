57ffc0f733d9e8591add72650c2c176a689b3b7f
周飞 <1174977961@qq.com>
2019-07-25 22:15:51 +0800

    add: 添加中介者类

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/对象去耦型/中介者模式/DPMediator.h
@@ -0,0 +1,18 @@
+//
+//  DPMediator.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/7/25.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+//  处理不同控制器的交互
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPMediator : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/对象去耦型/中介者模式/DPMediator.m
@@ -0,0 +1,13 @@
+//
+//  DPMediator.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/7/25.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPMediator.h"
+
+@implementation DPMediator
+
+@end
