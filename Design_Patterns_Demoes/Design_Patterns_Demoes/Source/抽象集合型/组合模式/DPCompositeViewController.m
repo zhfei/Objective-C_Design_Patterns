a230ec3b82106977258d225af943388ae5ff2f8a
zhoufei <1174977961@qq.com>
2019-07-29 23:34:31 +0800

    add: 添加组合模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/抽象集合型/组合模式/DPCompositeViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPCompositeViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by zhoufei on 2019/7/29.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPCompositeViewController.h"
+
+@interface DPCompositeViewController ()
+
+@end
+
+@implementation DPCompositeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
