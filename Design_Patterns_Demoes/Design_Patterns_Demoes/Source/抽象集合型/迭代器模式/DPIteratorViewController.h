99e8d0ca1c3cbf18102b664d882750f12a353251
zhoufei <1174977961@qq.com>
2019-08-04 23:50:22 +0800

    add: 添加迭代器模式

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/抽象集合型/迭代器模式/DPIteratorViewController.h
@@ -0,0 +1,17 @@
+//
+//  DPIteratorViewController.h
+//  Design_Patterns_Demoes
+//
+//  Created by zhoufei on 2019/8/4.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPIteratorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/抽象集合型/迭代器模式/DPIteratorViewController.m
@@ -0,0 +1,32 @@
+//
+//  DPIteratorViewController.m
+//  Design_Patterns_Demoes
+//
+//  Created by zhoufei on 2019/8/4.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPIteratorViewController.h"
+
+@interface DPIteratorViewController ()
+
+@end
+
+@implementation DPIteratorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
