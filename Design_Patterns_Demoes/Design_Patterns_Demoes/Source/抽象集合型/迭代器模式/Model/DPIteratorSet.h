0593e4d1c72ea798e86c7e666f68d4c90b080490
周飞 <1174977961@qq.com>
2019-08-05 23:51:49 +0800

    other:完善迭代器

---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/抽象集合型/迭代器模式/Model/DPIteratorSet.h
@@ -0,0 +1,17 @@
+//
+//  DPIteratorSet.h
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/8/5.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPIteratorSet : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Design_Patterns_Demoes/Design_Patterns_Demoes/Source/抽象集合型/迭代器模式/Model/DPIteratorSet.m
@@ -0,0 +1,13 @@
+//
+//  DPIteratorSet.m
+//  Design_Patterns_Demoes
+//
+//  Created by 周飞 on 2019/8/5.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "DPIteratorSet.h"
+
+@implementation DPIteratorSet
+
+@end
